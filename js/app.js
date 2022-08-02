263804179a388e6ea733ef793a59164fb88ba0e6
Eugene-CG <evhenisssss@gmail.com>
2022-08-02 19:07:11 +0300

    Froggy Game

--- js/app.js
+++ js/app.js
@@ -1,46 +1,112 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+const charactersSize = {
+  charboy: 50,
+  bug: 35,
+  sizeDiff: 15,
 };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+const tileSize = {
+  width: 101,
+  height: 85,
 };
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+const gameBoard = {
+  left: 0,
+  top: 0,
+  right: tileSize.width * 5,
+  bottom: tileSize.height * 6,
+};
+class Character {
+  constructor(x, y, sprite) {
+    this.x = x;
+    this.y = y;
+    this.sprite = sprite;
+  }
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+  update() {}
+}
+class Enemy extends Character {
+  constructor(x, y, speed) {
+    super(x, y, "images/enemy-bug.png");
+    this.speed = speed;
+  }
+  collision() {
+    if (
+      this.x +
+        tileSize.width -
+        charactersSize.charboy +
+        charactersSize.sizeDiff >
+        player.x &&
+      this.x - charactersSize.charboy - charactersSize.sizeDiff < player.x &&
+      this.y === player.y + charactersSize.sizeDiff
+    ) {
+      setTimeout(() => {
+        player.x = tileSize.width * 2;
+        player.y = tileSize.height * 4 - charactersSize.charboy;
+      }, 100);
+    }
+  }
+  update(dt) {
+    this.x += this.speed * dt;
+    if (this.x > 500) this.x = -tileSize.width;
+    this.collision();
+  }
+}
+class Player extends Character {
+  constructor(x, y) {
+    super(x, y, "images/char-boy.png");
+  }
+  reset() {
+    if (this.y < tileSize.height - charactersSize.charboy) {
+      setTimeout(() => {
+        alert("You won");
+        this.y = tileSize.height * 4 - charactersSize.charboy;
+        this.x = tileSize.width * 2;
+      }, 100);
+    }
+  }
+  handleInput(direction) {
+    if (direction === "left" && this.x > gameBoard.left) {
+      this.x -= tileSize.width;
+    }
+    if (direction === "up" && this.y > gameBoard.top) {
+      this.y -= tileSize.height;
+    }
+    if (direction === "right" && this.x < gameBoard.right - tileSize.width) {
+      this.x += tileSize.width;
+    }
+    if (
+      direction === "down" &&
+      this.y < gameBoard.bottom - tileSize.height * 2
+    ) {
+      this.y += tileSize.height;
+    }
+    this.reset();
+  }
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+const enemyLocationY = [
+  tileSize.height - charactersSize.bug,
+  tileSize.height * 2 - charactersSize.bug,
+  tileSize.height * 3 - charactersSize.bug,
+];
+const allEnemies = [];
 
+enemyLocationY.forEach((location) =>
+  allEnemies.push(new Enemy(100, location, (Math.random() + 1) * 100))
+);
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+const player = new Player(
+  tileSize.width * 2,
+  tileSize.height * 4 - charactersSize.charboy
+);
 
+document.addEventListener("keyup", function (e) {
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+  };
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
